74f9aeb059b7cc649f95bc78a28a3204d307ebfb
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2020-09-09 19:06:02 +0200

    adding old b1 and b1.h
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- subsys/dfu/src/dfu_target_b1.c
+++ subsys/dfu/src/dfu_target_b1.c
@@ -4,58 +4,85 @@
  * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
  */
 
+/*
+ * Ensure 'strnlen' is available even with -std=c99. If
+ * _POSIX_C_SOURCE was defined we will get a warning when referencing
+ * 'strnlen'. If this proves to cause trouble we could easily
+ * re-implement strnlen instead, perhaps with a different name, as it
+ * is such a simple function.
+ */
+#if !defined(_POSIX_C_SOURCE)
+#define _POSIX_C_SOURCE 200809L
+#endif
+#include <string.h>
+
 #include <zephyr.h>
 #include <pm_config.h>
 #include <logging/log.h>
-#include <dfu/mcuboot.h>
 #include <dfu/dfu_target.h>
 #include <dfu/dfu_target_stream.h>
 
 LOG_MODULE_REGISTER(dfu_target_b1, CONFIG_DFU_TARGET_LOG_LEVEL);
 
-bool dfu_target_b1_identify(const void *const buf)
-{
-	uint32_t reset_addr = ((uint32_t *)buf)[1];
-	return reset_addr  <= PM_B1_ADDRESS && \
-	       reset_addr > PM_B1_ADDRESS + PM_B1_SIZE;
-}
+#define MAX_FILE_SEARCH_LEN 500
 
-int dfu_target_b1_set_buf(u8_t *buf, len)
+int dfu_ctx_b1_set_file(const char *file, bool s0_active,
+				const char **update)
 {
-	if (buf == NULL) {
+	if (file == NULL || update == NULL) {
 		return -EINVAL;
 	}
 
-	stream_buf = buf;
-	stream_buf_len = len;
+	/* Ensure that 'file' is null-terminated. */
+	if (strnlen(file, MAX_FILE_SEARCH_LEN) == MAX_FILE_SEARCH_LEN) {
+		return -ENOTSUP;
+	}
+
+	/* We have verified that there is a null-terminator, so this is safe */
+	char *space = strstr(file, " ");
+
+	if (space == NULL) {
+		/* Could not find space separator in input */
+		*update = NULL;
+
+		return 0;
+	}
+
+	if (s0_active) {
+		/* Point after space to 'activate' second file path (S1) */
+		*update = space + 1;
+	} else {
+		*update = file;
+
+		/* Insert null-terminator to 'activate' first file path (S0) */
+		*space = '\0';
+	}
 
 	return 0;
+}
+
+bool dfu_target_b1_identify(const void *const buf)
+{
+	u32_t *vtable = (u32_t *)(buf);
+	u32_t reset_addr = vtable[1];
+	return (reset_addr > PM_S0_ADDR) &&
+	       (reset_addr < (PM_S1_ADDR + PM_S1_SIZE));
 }
 
 int dfu_target_b1_init(size_t file_size, dfu_target_callback_t cb)
 {
 	ARG_UNUSED(cb);
-	struct device *flash_dev;
-	int err;
+	int err = dfu_target_stream(file_size); /* TODO */
 
-	if (stream_buf == NULL) {
-		LOG_ERR("Missing stream_buf, call '..set_buf' before '..init");
-		return -ENODEV;
-	}
-
-	if (file_size > PM_B1_SIZE) {
-		LOG_ERR("Requested file too big to fit in flash %zu > 0x%x",
-			file_size, PM_B1_SIZE);
-		return -EFBIG;
-	}
-
-	flash_dev = device_get_binding(PM_B1_DEV_NAME);
-
-	err = dfu_target_stream_init(&stream_dfu, &stream,
-				     DFU_TARGET_STERAM_MCUBOOT);
-	if (err < 0) {
-		LOG_ERR("dfu_target_stream_init failed %d", err);
+	if (err != 0) {
+		LOG_ERR("stream_flash_init error %d", err);
 		return err;
+	}
+
+	if (file_size > PM_S0_SIZE) {
+		LOG_ERR("Requested file too big to fit in flash %zu > 0x%x",
+			file_size, PM_S0_SIZE);
+		return -EFBIG;
 	}
 
 	return 0;
