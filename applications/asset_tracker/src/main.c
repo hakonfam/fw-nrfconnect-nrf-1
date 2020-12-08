2f5fcb8071c1a927b27e9341d5be99a84ef844ea
Emanuele Di Santo <emdi@nordicsemi.no>
2020-12-08 12:39:58 +0100

    lib: bsdlib: rename to nrf_modem
    Rename bsdlib to nrf_modem (library) and nrf_modem_lib (integration).
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>
    Signed-off-by: Uma Praseeda <uma.praseeda@nordicsemi.no>

--- applications/asset_tracker/src/main.c
+++ applications/asset_tracker/src/main.c
@@ -1404,7 +1404,7 @@ void cloud_event_handler(const struct cloud_backend *const backend,
 		LOG_INF("CLOUD_EVT_READY");
 		ui_led_set_pattern(UI_CLOUD_CONNECTED);
 
-#if defined(CONFIG_BOOTLOADER_MCUBOOT) && !defined(CONFIG_NRF_CLOUD_FOTA)
+#if defined(CONFIG_BOOTLOADER_MCUBOOT)
 		/* Mark image as good to avoid rolling back after update */
 		boot_write_img_confirmed();
 #endif
@@ -1489,18 +1489,6 @@ void connection_evt_handler(const struct cloud_event *const evt)
 		switch (evt->data.err) {
 		case CLOUD_DISCONNECT_INVALID_REQUEST:
 			LOG_INF("Cloud connection closed.");
-			break;
-		case CLOUD_DISCONNECT_USER_REQUEST:
-			if (atomic_get(&cloud_association) ==
-			    CLOUD_ASSOCIATION_STATE_RECONNECT ||
-			    atomic_get(&cloud_association) ==
-			    CLOUD_ASSOCIATION_STATE_REQUESTED ||
-			    (atomic_get(&carrier_requested_disconnect))) {
-				connect_wait_s = 10;
-			}
-			break;
-		case CLOUD_DISCONNECT_CLOSED_BY_REMOTE:
-			LOG_INF("Disconnected by the cloud.");
 			if ((atomic_get(&cloud_connect_attempts) == 1) &&
 			    (atomic_get(&cloud_association) ==
 			     CLOUD_ASSOCIATION_STATE_INIT)) {
@@ -1516,9 +1504,20 @@ void connection_evt_handler(const struct cloud_event *const evt)
 #endif
 				connect_wait_s = 10;
 			} else {
-				LOG_INF("This can occur if the device has the wrong nRF Cloud certificates");
-				LOG_INF("or if the device has been removed from nRF Cloud.");
+				LOG_INF("This can occur if the device has the wrong nRF Cloud certificates.");
 			}
+			break;
+		case CLOUD_DISCONNECT_USER_REQUEST:
+			if (atomic_get(&cloud_association) ==
+			    CLOUD_ASSOCIATION_STATE_RECONNECT ||
+			    atomic_get(&cloud_association) ==
+			    CLOUD_ASSOCIATION_STATE_REQUESTED ||
+			    (atomic_get(&carrier_requested_disconnect))) {
+				connect_wait_s = 10;
+			}
+			break;
+		case CLOUD_DISCONNECT_CLOSED_BY_REMOTE:
+			LOG_INF("Disconnected by the cloud.");
 			break;
 		case CLOUD_DISCONNECT_MISC:
 		default:
@@ -1791,7 +1790,7 @@ static void ui_evt_handler(struct ui_evt evt)
 
 void handle_nrf_modem_lib_init_ret(void)
 {
-#if defined(CONFIG_NRF_MODEM_LIB) && !defined(CONFIG_NRF_CLOUD_FOTA)
+#if defined(CONFIG_NRF_MODEM_LIB)
 	int ret = nrf_modem_lib_get_init_ret();
 
 	/* Handle return values relating to modem firmware update */
