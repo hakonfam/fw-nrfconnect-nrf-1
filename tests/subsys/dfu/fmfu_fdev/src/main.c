8a40082b7f0d547d4003b5fcb6a6b7f0972a9d0b
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2020-12-17 14:00:02 +0100

    dump
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- tests/subsys/dfu/fmfu_fdev/src/main.c
+++ tests/subsys/dfu/fmfu_fdev/src/main.c
@@ -12,16 +12,42 @@
 #include <drivers/flash.h>
 #include <dfu/fmfu_fdev.h>
 
-static void test_fmfu_fdev_prevalidate(void)
-{
-	zassert_true(true, NULL);
+#define FLASH_NAME DT_CHOSEN_ZEPHYR_FLASH_CONTROLLER_LABEL
 
+static const struct device *fdev;
+static uint8_t buf[128];
+
+static void test_fmfu_fdev_load_null(void)
+{
+	int err;
+
+	err = fmfu_fdev_load(NULL, sizeof(buf), fdev, 0);
+	zassert_true(err < 0, "Expected negative error code for NULL argument");
+
+	err = fmfu_fdev_load(buf, sizeof(buf), NULL, 0);
+	zassert_true(err < 0, "Expected negative error code for NULL argument");
+}
+
+static void test_fmfu_fdev_load_invalid_offset(void)
+{
+	int err;
+
+	err = fmfu_fdev_load(buf, sizeof(buf), fdev, 0x12345678);
+	zassert_true(err < 0,
+		     "Expected negative error code for invalid offset");
+
+	err = fmfu_fdev_load(buf, sizeof(buf), fdev, 0x10000);
+	zassert_true(err < 0,
+		     "Expected negative error code for invalid offset");
 }
 
 void test_main(void)
 {
+	fdev = device_get_binding(FLASH_NAME);
+
 	ztest_test_suite(lib_fmfu_fdev_test,
-	     ztest_unit_test(test_fmfu_fdev_prevalidate)
+	     ztest_unit_test(test_fmfu_fdev_load_null),
+	     ztest_unit_test(test_fmfu_fdev_load_invalid_offset)
 	 );
 
 	ztest_run_test_suite(lib_fmfu_fdev_test);
