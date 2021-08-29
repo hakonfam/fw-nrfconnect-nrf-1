fe1e737700b64cccd3fce96ded74b09caf7ad925
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2021-08-29 14:22:05 +0200

    nrf53: Add support for multi image DFU
    Some long descriptive text.
    
    ini
    
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

---
+++ modules/mcuboot/hooks/nrf53_hooks.c
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <assert.h>
+#include <zephyr.h>
+#include <device.h>
+#include <drivers/flash/flash_simulator.h>
+#include "bootutil/image.h"
+#include "bootutil/bootutil.h"
+#include "bootutil/fault_injection_hardening.h"
+#include "flash_map_backend/flash_map_backend.h"
+
+#define NET_CORE_SECONDARY_SLOT 1
+#define NET_CORE_VIRTUAL_PRIMARY_SLOT 3
+
+#include <dfu/pcd.h>
+
+/* @retval 0: header was read/populated
+ *         FIH_FAILURE: image is invalid,
+ *         BOOT_HOOK_REGULAR if hook not implemented for the image-slot,
+ *         othervise an error-code value.
+ */
+int boot_read_image_header_hook(int img_index, int slot,
+		struct image_header *img_head)
+{
+	if (img_index == 1 && slot == 0) {
+		img_head->ih_magic = IMAGE_MAGIC;
+		return 0;
+	}
+
+	return BOOT_HOOK_REGULAR;
+}
+
+/* @retval FIH_SUCCESS: image is valid,
+ *         FIH_FAILURE: image is invalid,
+ *         fih encoded BOOT_HOOK_REGULAR if hook not implemented for
+ *         the image-slot.
+ */
+fih_int boot_image_check_hook(int img_index, int slot)
+{
+	if (img_index == 1 && slot == 0) {
+		FIH_RET(FIH_SUCCESS);
+	}
+
+	FIH_RET(fih_int_encode(BOOT_HOOK_REGULAR));
+}
+
+int boot_perform_update_hook(int img_index, struct image_header *img_head,
+		const struct flash_area *area)
+{
+	return BOOT_HOOK_REGULAR;
+}
+
+int boot_read_swap_state_primary_slot_hook(int image_index,
+		struct boot_swap_state *state)
+{
+	if (image_index == 1) {
+		/* Populate with fake data */
+		state->magic = BOOT_MAGIC_UNSET;
+		state->swap_type = BOOT_SWAP_TYPE_NONE;
+		state->image_num = image_index;
+		state->copy_done = BOOT_FLAG_UNSET;
+		state->image_ok = BOOT_FLAG_UNSET;
+
+		/*
+		 * Skip more handling of the primary slot for Image 1 as the slot
+		 * exsists in RAM and is empty.
+		 */
+		return 0;
+	}
+
+	return BOOT_HOOK_REGULAR;
+}
+
+int network_core_update(void)
+{
+	struct image_header *hdr;
+	static const struct device *mock_flash_dev;
+	void *mock_flash;
+	size_t mock_size;
+
+	mock_flash_dev = device_get_binding(PM_MCUBOOT_PRIMARY_1_DEV_NAME);
+	if (mock_flash_dev == NULL) {
+		return -ENODEV;
+	}
+
+	mock_flash = flash_simulator_get_memory(NULL ,&mock_size);
+	hdr = (struct image_header *) mock_flash;
+	if (hdr->ih_magic == IMAGE_MAGIC) {
+		uint32_t fw_size = hdr->ih_img_size;
+		uint32_t vtable_addr = (uint32_t)hdr + hdr->ih_hdr_size;
+		uint32_t *vtable = (uint32_t *)(vtable_addr);
+		uint32_t reset_addr = vtable[1];
+		if (reset_addr > PM_CPUNET_B0N_ADDRESS) {
+			int rc = pcd_network_core_update(vtable, fw_size);
+			return rc;
+		}
+	}
+
+	/* No IMAGE_MAGIC no valid image */
+	return -ENODATA;
+}
+
+int boot_copy_region_post_hook(int img_index, const struct flash_area *area,
+		size_t size)
+{
+	if (img_index == NET_CORE_SECONDARY_SLOT) {
+		return network_core_update();
+	}
+
+	return 0;
+}
+
+int boot_serial_uploaded_hook(int img_index, const struct flash_area *area,
+		size_t size)
+{
+	if (img_index == NET_CORE_VIRTUAL_PRIMARY_SLOT) {
+		return network_core_update();
+	}
+
+	return 0;
+}
